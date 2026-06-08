fcacb84fb2bdab852ffa2b84c81f81ca51e243ed
Saga <sagarikarai6@gmail.com>
2026-06-08 08:57:23 +0200

    Add headless Chrome options for Jenkins execution

--- src/test/java/seleniumproject/testcomponents/BaseTest.java
+++ src/test/java/seleniumproject/testcomponents/BaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -26,10 +27,12 @@ public class BaseTest {
         }
     }
 
-	public WebDriver initializeDriver() {
-	    WebDriverManager.chromedriver().setup();
-	    driver = new ChromeDriver(); 
+	public WebDriver initializeDriver() { 
 	    
+	    WebDriverManager.chromedriver().setup();
+	    ChromeOptions options = new ChromeOptions();
+	    
+	    driver = new ChromeDriver(options); 
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	    driver.manage().window().maximize();
 	    
