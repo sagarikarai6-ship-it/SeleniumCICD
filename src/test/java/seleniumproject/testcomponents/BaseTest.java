5ce343f48dadda2a39d5262da5a2a5ef2f7d7cd4
Saga <sagarikarai6@gmail.com>
2026-06-08 10:37:55 +0200

    Implement standalone Selenium Chrome container via Docker Compose

--- src/test/java/seleniumproject/testcomponents/BaseTest.java
+++ src/test/java/seleniumproject/testcomponents/BaseTest.java
@@ -1,10 +1,13 @@
 package seleniumproject.testcomponents;
 
+import java.net.URI;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -29,12 +32,29 @@ public class BaseTest {
 
 	public WebDriver initializeDriver() { 
 	    
-	    WebDriverManager.chromedriver().setup();
+		String remoteUrl = System.getProperty("selenium.remote.url");
+	    //WebDriverManager.chromedriver().setup();
 	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless=new");
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
+	    options.addArguments("--window-size=1920,1080");
 	    
-	    driver = new ChromeDriver(options); 
+	    if (remoteUrl != null) {
+	        try {
+	            // Point to the dedicated Docker  container using the modern URI conversion method
+	            driver = new RemoteWebDriver(URI.create(remoteUrl).toURL(), options);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }else {
+	        // Fallback for when you run locally inside Eclipse UI
+	        WebDriverManager.chromedriver().setup();
+	        driver = new ChromeDriver(options);
+	    }
+	    //driver = new ChromeDriver(options); 
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	    driver.manage().window().maximize();
+	    //driver.manage().window().maximize();
 	    
 	    return driver;
 	}
